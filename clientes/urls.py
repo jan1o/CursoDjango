9be204eb5b40e7357605fdb8a56e23f8877ea42c
Janio Fernandes <juniorjanio2016@gmail.com>
2021-01-26 15:57:37 -0200

    aula 18

--- clientes/urls.py
+++ clientes/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import persons_list
+from .views import *
 
 
 
 urlpatterns = [
-    path('list/', persons_list),
-
+    path('list/', persons_list, name="persons_list"),
+    path('new/', persons_new, name="persons_new"),
 ]
--- clientes/views.py
+++ clientes/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from .forms import PersonForm
 
 # Create your views here.
 def persons_list(request):
     persons = Person.objects.all()
     return render(request, 'person.html', {'persons': persons})
+
+def persons_new(request):
+    form = PersonForm(request.POST, request.FILES, None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('persons_list')
+    return render(request, 'person_form.html', {'form': form})
